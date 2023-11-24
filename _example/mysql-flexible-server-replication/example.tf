ac00043c3a97ae92b6994717b69e9f301f0f9259
chandrahaskoneru <123540688+chandrahaskoneru@users.noreply.github.com>
2023-11-24 11:30:26 +0530

    Update example.tf

--- _example/mysql-flexible-server-replication/example.tf
+++ _example/mysql-flexible-server-replication/example.tf
@@ -9,7 +9,7 @@ locals {
 }
 
 module "resource_group" {
-  source      = "git::https://github.com/opz0/terraform-azure-resource-group.git?ref=v1.0.0"
+  source      = "git::https://github.com/cypik/terraform-azure-resource-group.git?ref=v1.0.0"
   name        = "app"
   environment = "flexible"
   location    = "North Europe"
@@ -17,7 +17,7 @@ module "resource_group" {
 
 
 module "vnet" {
-  source              = "git::https://github.com/opz0/terraform-azure-vnet.git?ref=v1.0.0"
+  source              = "git::https://github.com/cypik/terraform-azure-vnet.git?ref=v1.0.0"
   name                = "app"
   environment         = "testnew"
   resource_group_name = module.resource_group.resource_group_name
@@ -26,7 +26,7 @@ module "vnet" {
 }
 
 module "subnet" {
-  source = "git::https://github.com/opz0/terraform-azure-subnet.git?ref=v1.0.0"
+  source = "git::https://github.com/cypik/terraform-azure-subnet.git?ref=v1.0.0"
 
   name                 = "app"
   environment          = "test"
